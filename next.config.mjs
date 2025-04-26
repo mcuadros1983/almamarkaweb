89b80cdc8768aa0a213b2ce9747c4dfbb001e932
mcuadros <mcuadros83@gmail.com>
2025-04-26 00:10:31 -0300

    se agrega redireccion

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'almamarka.com', // Detecta cuando entran sin www
+            },
+          ],
+          destination: 'https://www.almamarka.com/:path*', // Redirige al www
+          permanent: true, // Código 301 para SEO
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
