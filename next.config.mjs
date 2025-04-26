c78ff97901a7eba4a248b15fb3ea927f87b3fa60
mcuadros <mcuadros83@gmail.com>
2025-04-26 20:58:28 -0300

    corrige next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'almamarka.com', // Detecta cuando entran sin www
-            },
-          ],
-          destination: 'https://www.almamarka.com/:path*', // Redirige al www
-          permanent: true, // Código 301 para SEO
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'standalone', // 👈 Agregado para producción correcta en Railway
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'almamarka.com', // Detecta cuando entran sin www
+          },
+        ],
+        destination: 'https://www.almamarka.com/:path*', // Redirige al www
+        permanent: true, // Código 301 para SEO
+      },
+    ];
+  },
+};
+
+export default nextConfig;
