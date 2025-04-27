f4fca8bb6d7d991684ae045fc4af9aa9d2360816
mcuadros <mcuadros83@gmail.com>
2025-04-26 22:23:26 -0300

    se modifica sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   siteUrl: 'https://www.almamarka.com',
   generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+    await config.transform(config, '/condiciones-reserva'),
+    // Agregá más páginas si querés: 
+    // await config.transform(config, '/galeria'),
+    // await config.transform(config, '/contacto'),
+  ],
 };
