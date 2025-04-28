9eff17c75e3aec147739fb5d6f8b623a62bfed2a
mcuadros <mcuadros83@gmail.com>
2025-04-28 11:07:25 -0300

    se elimina standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone', // 👈 Agregado para producción correcta en Railway
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'almamarka.com', // Detecta cuando entran sin www
-          },
-        ],
-        destination: 'https://www.almamarka.com/:path*', // Redirige al www
-        permanent: true, // Código 301 para SEO
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
-
